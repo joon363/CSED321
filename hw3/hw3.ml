6aaeb42ca029613109235065048233dffef4ce95
joon363 <joon363@naver.com>
2025-03-12 05:48:40 +0900

    implemented Stage 1

--- hw3/hw3.ml
+++ hw3/hw3.ml
@@ -32,9 +32,9 @@ struct
   let zero = false
   let one = true
 
-  let (++) x y = raise NotImplemented
-  let ( ** ) x y = raise NotImplemented
-  let (==) x y = raise NotImplemented
+  let (++) x y = (x || y)
+  let ( ** ) x y = (x && y)
+  let (==) x y = (x = y)
 end
 
 (* Problem 1-2 *)
@@ -44,17 +44,29 @@ module VectorFn (Scal : SCALAR) : VECTOR with type elem = Scal.t
 =
 struct
   type elem = Scal.t
-  type t = unit
+  type t = elem list
 
   exception VectorIllegal
 
-  let create _ = raise NotImplemented
-  let to_list _ = raise NotImplemented
-  let dim _ = raise NotImplemented
-  let nth _ = raise NotImplemented
-  let (++) _ _ = raise NotImplemented
-  let (==) _ _ = raise NotImplemented
-  let innerp _ _ = raise NotImplemented
+  let create lst = match lst with
+    | [] -> raise VectorIllegal
+    | _ -> lst
+  let to_list v = v
+  let dim v = List.length(v)
+  let rec nth v n = 
+    try List.nth v n
+    with _ -> raise VectorIllegal
+
+  let (++) v1 v2 = 
+    if dim v1 != dim v2 then raise VectorIllegal
+    else List.map2 (fun x y -> Scal.(++) x y) v1 v2
+  let (==) v1 v2 = 
+    if dim v1 != dim v2 then raise VectorIllegal
+    else List.equal (fun x y -> Scal.(==) x y) v1 v2 
+  let innerp v1 v2= 
+    if dim v1 != dim v2 then raise VectorIllegal
+    else List.fold_left 
+      (fun a b -> Scal.(++) a b) (Scal.zero) (List.map2 (fun x y -> Scal.( ** ) x y) v1 v2)
 end
 
 (* Problem 1-3 *)
@@ -63,21 +75,49 @@ end
 module MatrixFn (Scal : SCALAR) : MATRIX with type elem = Scal.t
 =
 struct
+  
   type elem = Scal.t
-  type t = unit
+  type t = elem list list
 
   exception MatrixIllegal
 
-  let create _ = raise NotImplemented
-  let identity _ = raise NotImplemented
-  let dim _ = raise NotImplemented
-  let transpose _ = raise NotImplemented
-  let to_list _ = raise NotImplemented
-  let get _ _ _ = raise NotImplemented 
+  let create lst = match lst with
+    | [] -> raise MatrixIllegal
+    | _ -> List.map (fun row -> if (List.length row = List.length lst) then row else raise MatrixIllegal) lst
+  let identity n = 
+    if n <=0 then raise MatrixIllegal 
+    else List.init n (fun i -> List.init n (fun j -> if i = j then Scal.one else Scal.zero))
+  let dim mat = List.length mat
+  let transpose mat = 
+    List.init (dim mat) 
+      (fun newRowIndex -> (List.init (dim mat) (fun newColumnIndex -> List.nth(List.nth mat newColumnIndex) newRowIndex)))
+      (*newRowIndex = column index of original mat,
+        newColumnIndex = row index of original mat
+        therefore List.nth(List.nth mat newColumnIndex) newRowIndex is actually mat[row][col]*)
+  let to_list mat = mat
+  let get m r c =
+    try List.nth (List.nth m r) c
+    with _ -> raise MatrixIllegal 
 
-  let (++) _ _ = raise NotImplemented
-  let ( ** ) _ _ = raise NotImplemented
-  let (==) _ _ = raise NotImplemented
+  let (++) m1 m2 =
+  if dim m1 != dim m2 then raise MatrixIllegal
+  else 
+    List.map2 
+      (fun row1 row2 -> (List.map2 (fun x y -> Scal.(++) x y) row1 row2)) 
+      m1 m2
+
+  let ( ** ) m1 m2 =
+    if dim m1 != dim m2 then raise MatrixIllegal
+    else 
+    List.map (fun row1 ->
+      List.map (fun row2 ->
+        List.fold_left (fun acc (x, y) -> Scal.(acc ++ (x ** y))) Scal.zero (List.combine row1 row2)
+      ) (transpose m2)
+    ) m1
+
+  let (==) m1 m2 =
+    if dim m1 != dim m2 then raise MatrixIllegal
+    else List.for_all2 (fun row1 row2 -> List.equal (fun x y -> Scal.(==) x y) row1 row2) m1 m2
 end
 
 (* Problem 2-1 *)
@@ -193,4 +233,4 @@ let _ =
   else
     print_endline "\nYour program might have bugs!"
   with _ -> print_endline "\nYour program is not complete yet!" 
-
+      
