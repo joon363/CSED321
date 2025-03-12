e01880673150dba3a1819c120f54a4003e2efd68
joon363 <joon363@naver.com>
2025-03-12 17:12:16 +0900

    Fixed Test Logic

--- hw3/hw3.ml
+++ hw3/hw3.ml
@@ -19,6 +19,8 @@ struct
   let (==) x y = x = y 
 end
 
+
+  
 (* Problem 1-1 *)
 (* Scalars *)
 
@@ -234,3 +236,162 @@ let _ =
     print_endline "\nYour program might have bugs!"
   with _ -> print_endline "\nYour program is not complete yet!" 
       
+
+
+
+
+
+let print_test text condition = 
+  Printf.printf "%s: %s\n" text (if condition then "pass" else "fail")
+
+
+let test_integer () =
+  try
+    print_test "Zero" (Integer.zero = 0);
+    print_test "One" (Integer.one = 1);
+    print_test "Addition (10 ++ 6)" ((Integer.(++) 10 16) = 26);
+    print_test "Multiplication (10 ++ 6)" ((Integer.( ** ) 10 16) = 160);
+    print_test "Equality (3 == 4)" ((Integer.(==) 3 4) = false);
+    print_test "Equality (1 == 1)" ((Integer.(==) 1 1) = true);
+
+  with
+  | Integer.ScalarIllegal -> Printf.printf "Test failed with ScalarIllegal exception\n"
+  | _ -> Printf.printf "Test failed with unexpected exception\n"
+
+let _ = Printf.printf "=============Test Integer=============\n"; test_integer()
+
+let test_boolean () =
+  try
+    (* Test for OR (++) *)
+    print_test "Addition (true ++ false)" (Boolean.(++) true false = true);
+    print_test "Addition (false ++ false)" (Boolean.(++) false false = false);
+    print_test "Addition (true ++ true)" (Boolean.(++) true true = true);
+
+    (* Test for AND (**) *)
+    print_test "Multiplication (true ** false)" (Boolean.( ** ) true false = false);
+    print_test "Multiplication (false ** false)" (Boolean.( ** ) false false = false);
+    print_test "Multiplication (true ** true)" (Boolean.( ** ) true true = true);
+
+    (* Test for equality (==) *)
+    print_test "Equality (true == true)" (Boolean.( == ) true true = true);
+    print_test "Equality (false == true)" (Boolean.( == ) false true = false);
+    print_test "Equality (false == false)" (Boolean.( == ) false false = true);
+  with
+  | Boolean.ScalarIllegal -> Printf.printf "Test failed with ScalarIllegal exception\n"
+  | _ -> Printf.printf "Test failed with unexpected exception\n"
+
+
+  let _ = Printf.printf "=============Test Boolean=============\n"; test_boolean()
+
+  
+  
+  (* Create the VectorFn module for Boolean *)
+  module BooleanVector = VectorFn(Boolean)
+  
+  let test_vector () =
+    try
+      (* Define some vectors using the created module *)
+      let v1 = BooleanVector.create [true; false; true] in
+      let v2 = BooleanVector.create [false; true; true] in
+      let v3 = BooleanVector.create [true; true; true] in
+      let addv = BooleanVector.create [true; true; true] in
+  
+      (* Test for vector addition (++) *)
+      let sum_v1_v2 = BooleanVector.(++) v1 v2 in
+      print_test "Addition (v1 ++ v2)" (BooleanVector.(==) sum_v1_v2 addv);
+  
+      (* Test for vector equality (==) *)
+      print_test "Equality (v1 == v2)" (BooleanVector.(==) v1 v2 = false);
+      print_test "Equality (v2 == v2)" (BooleanVector.(==) v2 v2 = true);
+  
+      (* Test for inner product (innerp) *)
+      let inner_v1_v2 = BooleanVector.innerp v1 v2 in
+      print_test "Inner product (v1 . v2)" (inner_v1_v2 = true);
+  
+      let inner_v1_v3 = BooleanVector.innerp v1 v3 in
+      print_test "Inner product (v1 . v3)" (inner_v1_v3 = true);
+  
+    with
+    | BooleanVector.VectorIllegal -> Printf.printf "Test failed with VectorIllegal exception\n"
+    | _ -> Printf.printf "Test failed with unexpected exception\n"
+
+    
+let _ = Printf.printf "=============Test Vector=============\n"; test_vector()
+
+
+
+
+module Matrix = MatrixFn(Integer)
+
+let test_matrix () =
+  try
+    (* 1. Matrix creation and identity matrix *)
+    let m1 = Matrix.create [[1; 0]; [0; 1]] in
+    let m2 = Matrix.create [[0; 1]; [1; 0]] in
+    let identity_matrix = Matrix.identity 2 in
+
+    (* Test identity matrix *)
+    print_test "Identity matrix (2x2)" (Matrix.(==) identity_matrix (Matrix.identity 2));
+
+    (* 2. Matrix addition (++) *)
+    let sum = Matrix.(++) m1 m2 in
+    let expected_sum = Matrix.create [[1; 1]; [1; 1]] in
+    print_test "Matrix addition (m1 ++ m2)" (Matrix.(==) sum expected_sum);
+
+    (* 3. Matrix multiplication (**) *)
+    let m3 = Matrix.create [[1; 0]; [0; 1]] in
+    let m4 = Matrix.create [[1; 0]; [0; 1]] in
+    let mul_result = Matrix.( ** ) m3 m4 in
+    let expected_mul = Matrix.create [[1; 0]; [0; 1]] in
+    print_test "Matrix multiplication (m3 ** m4)" (Matrix.(==) mul_result expected_mul);
+
+    (* 4. Matrix transpose *)
+    let m5 = Matrix.create [[1; 0]; [0; 1]] in
+    let transposed = Matrix.transpose m5 in
+    print_test "Matrix transpose (m5)" (Matrix.(==) transposed m5);
+
+    (* 5. Matrix equality (==) *)
+    let equal = Matrix.(==) m1 m2 in
+    print_test "Matrix equality (m1 == m2)" (equal = false);
+
+  with
+  | Matrix.MatrixIllegal -> Printf.printf "Test failed with MatrixIllegal exception\n"
+  | _ -> Printf.printf "Test failed with unexpected exception\n"
+
+
+let test_matrix_exception1() = 
+  try
+    let invalid_m = Matrix.create [[1; 0]; [0]] in
+    print_test "Invalid matrix creation" false
+  with
+  | Matrix.MatrixIllegal -> print_test "Invalid matrix creation" true
+  | _ -> Printf.printf "Test failed with unexpected exception\n"
+  
+let test_matrix_exception2() =
+  try
+    let invalid_m1 = Matrix.create [[1]; [0]] in
+    let invalid_m2 = Matrix.create [[0; 1]; [1; 0]] in
+    let invalid_res = Matrix.(++) invalid_m1 invalid_m2 in
+    print_test "Invalid matrix addition" false
+  with
+  | Matrix.MatrixIllegal -> print_test "Invalid matrix addition" true
+  | _ -> Printf.printf "Test failed with unexpected exception\n"
+  
+let test_matrix_exception3() =
+  try
+    let invalid_m1 = Matrix.create [[1]; [0]] in
+    let invalid_m2 = Matrix.create [[0; 1]; [1; 0]] in
+    let invalid_res = Matrix.( ** ) invalid_m1 invalid_m2 in
+    print_test "Invalid matrix multiplication" false
+  with
+  | Matrix.MatrixIllegal -> print_test "Invalid matrix multiplication" true
+  | _ -> Printf.printf "Test failed with unexpected exception\n"
+  
+
+let matrix_test_all() = 
+  test_matrix(); 
+  test_matrix_exception1();
+  test_matrix_exception2();
+  test_matrix_exception3()
+
+let _ = Printf.printf "=============Test Integer Matrix=============\n"; matrix_test_all()
